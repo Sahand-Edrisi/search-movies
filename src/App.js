3658f562f2788bd11bfe284b7dad36eae9f45f4d
sahandedc <sahandedc@gmail.com>
2024-11-19 16:12:28 +0330

    test

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import { Route, Routes} from "react-router-dom";
 import MoviesShow from "./components/MoviesShow";
 
 const App = () => {
+ 
   return (
     <>
       <Routes>
-        <Route path="/search-movies/"  Component={MoviesShow} />
+        <Route path="/"  Component={MoviesShow} />
         <Route path="/search-movies/:id"  Component={MoviesShow} />
       </Routes>
     </>
