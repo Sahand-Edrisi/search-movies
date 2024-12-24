5ab3512a281d0b73138dbdb27ccb0f0ee5482e18
sahandedc <sahandedc@gmail.com>
2024-12-24 17:56:32 +0330

    Fix : update

--- src/components/getDataMovie.jsx
+++ src/components/getDataMovie.jsx
@@ -4,7 +4,7 @@ import MoviesShow from "./moviesShow";
 import Movie from "./movie";
 import NotFound from "./notFound";
 import notPicture from "../image/notPicture.jpg";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams, useNavigate, Link } from "react-router-dom";
 
 const GetDataMovie = () => {
   const params = useParams();
@@ -33,8 +33,8 @@ const GetDataMovie = () => {
 
   // best Movies data
   const [bestMovies, setBestMovies] = useState([]);
-  const [ShowBestMovies, setShowBestMovies] = useState(undefined);
-  const BestMoviesData = bestMovies.map((i, index) => ({
+  const [showBestMovies, setShowBestMovies] = useState(undefined);
+  const bestMoviesData = bestMovies.map((i, index) => ({
     name: i.show.name,
     image: i.show.image ? i.show.image.medium : notPicture,
     imageOriginal: i.show.image ? i.show.image.original : notPicture,
@@ -52,11 +52,41 @@ const GetDataMovie = () => {
   }));
 
   // search Movies data
-  const [ShowSearchMovies, setShowSearchMovies] = useState([]);
-  const showMoviesInSearch = ShowSearchMovies.map((f, index) => ({
-    key: index,
-    name: f.show.name,
-    imageOriginal: f.show.image ? f.show.image.medium : notPicture,
+  const [showSearchMovies, setShowSearchMovies] = useState([]);
+  const [showSearchMoviesItem, setShowSearchMoviesItem] = useState(undefined);
+  const showMoviesInSearch = showSearchMovies.map((i, index) => ({
+    name: i.show.name,
+    image: i.show.image ? i.show.image.medium : notPicture,
+    imageOriginal: i.show.image ? i.show.image.original : notPicture,
+    genres: i.show.genres ? i.show.genres.join(",") : undefined,
+    visitSite: i.show.url,
+    officialSite: i.show.officialSite,
+    language: i.show.language,
+    rating: i.show.rating.average ? i.show.rating.average : undefined,
+    summary: i.show.summary ? i.show.summary : undefined,
+    country: i.show.network !== null ? i.show.network.country.name : undefined,
+    status: i.show.status !== null ? i.show.status : undefined,
+    externals:
+      i.show.externals.imdb !== null ? i.show.externals.imdb : undefined,
+    id: index,
+  }));
+  // save search Movies data
+  const [showSearchMoviesItemData, setShowSearchMoviesItemData] = useState([]);
+  const SaveSearchMoviesItemData = showSearchMoviesItemData.map((i, index) => ({
+    name: i.show.name,
+    image: i.show.image ? i.show.image.medium : notPicture,
+    imageOriginal: i.show.image ? i.show.image.original : notPicture,
+    genres: i.show.genres ? i.show.genres.join(",") : undefined,
+    visitSite: i.show.url,
+    officialSite: i.show.officialSite,
+    language: i.show.language,
+    rating: i.show.rating.average ? i.show.rating.average : undefined,
+    summary: i.show.summary ? i.show.summary : undefined,
+    country: i.show.network !== null ? i.show.network.country.name : undefined,
+    status: i.show.status !== null ? i.show.status : undefined,
+    externals:
+      i.show.externals.imdb !== null ? i.show.externals.imdb : undefined,
+    id: index,
   }));
 
   useEffect(() => {
@@ -93,12 +123,19 @@ const GetDataMovie = () => {
             />
             <div id="ShowSearchMoviesOff">
               {showMoviesInSearch.map((i, index) => (
-                <div key={index} className="items">
-                  <img src={i.imageOriginal} alt="" />
-                  <p className="names" key={index}>
-                    {nameDataSlice(i.name)}
-                  </p>
-                </div>
+                <Link>
+                  <div
+                    key={index}
+                    className="items"
+                    onClick={showItem}
+                    id={index}
+                  >
+                    <img src={i.imageOriginal} alt="" />
+                    <p className="names" key={index}>
+                      {nameDataSlice(i.name)}
+                    </p>
+                  </div>
+                </Link>
               ))}
             </div>
           </div>
@@ -115,24 +152,58 @@ const GetDataMovie = () => {
         </div>
       </div>
       {vpn === true ? (
-        ShowBestMovies === undefined ? (
-          params.id === undefined ? (
-            data.length >= 3 ? (
-              <>
-                {/* bestMovies */}
-                <div id="bestMovies">
-                  <button className="btnLeft" onClick={btnLeft}>
-                    <i className="fa-solid fa-arrow-left"></i>
-                  </button>
-                  <div id="images" ref={BestMovieParentElement}>
-                    {createBestMovies()}
+        showSearchMoviesItem === undefined ? (
+          showBestMovies === undefined ? (
+            params.id === undefined ? (
+              data.length >= 3 ? (
+                <>
+                  {/* bestMovies */}
+                  <div id="bestMovies">
+                    <button className="btnLeft" onClick={btnLeft}>
+                      <i className="fa-solid fa-arrow-left"></i>
+                    </button>
+                    <div id="images" ref={BestMovieParentElement}>
+                      {createBestMovies()}
+                    </div>
+                    <button className="btnRight" onClick={btnRight}>
+                      <i className="fa-solid fa-arrow-right"></i>
+                    </button>
                   </div>
-                  <button className="btnRight" onClick={btnRight}>
-                    <i className="fa-solid fa-arrow-right"></i>
-                  </button>
-                </div>
 
-                {/* MoviesShow */}
+                  {/* MoviesShow */}
+                  <div id="MoviesShow">
+                    {data.map((i, index) => (
+                      <MoviesShow
+                        key={index}
+                        name={i.name}
+                        image={i.imageOriginal ? i.imageOriginal : i.image}
+                        genres={i.genres}
+                        visitSite={i.visitSite}
+                        officialSite={i.officialSite}
+                        language={i.language}
+                        rating={i.rating}
+                        id={i.id}
+                        summary={i.summary}
+                      />
+                    ))}
+                  </div>
+                  {/* Pagination */}
+                  <div className="containerPagination">
+                    <div className="titlePagination">
+                      <p>Movies pages</p>
+                    </div>
+                    <div className="paginationShow">
+                      <button id="previous" onClick={previousPage}>
+                        Previous
+                      </button>
+                      <div id="pages">{createPagination()}</div>
+                      <button id="next" onClick={nextPage}>
+                        Next
+                      </button>
+                    </div>
+                  </div>
+                </>
+              ) : (
                 <div id="MoviesShow">
                   {data.map((i, index) => (
                     <MoviesShow
@@ -140,8 +211,6 @@ const GetDataMovie = () => {
                       name={i.name}
                       image={i.imageOriginal ? i.imageOriginal : i.image}
                       genres={i.genres}
-                      visitSite={i.visitSite}
-                      officialSite={i.officialSite}
                       language={i.language}
                       rating={i.rating}
                       id={i.id}
@@ -149,41 +218,31 @@ const GetDataMovie = () => {
                     />
                   ))}
                 </div>
-                {/* Pagination */}
-                <div className="containerPagination">
-                  <div className="titlePagination">
-                    <p>Movies pages</p>
-                  </div>
-                  <div className="paginationShow">
-                    <button id="previous" onClick={previousPage}>
-                      Previous
-                    </button>
-                    <div id="pages">{createPagination()}</div>
-                    <button id="next" onClick={nextPage}>
-                      Next
-                    </button>
-                  </div>
-                </div>
-              </>
+              )
             ) : (
-              <div id="MoviesShow">
-                {data.map((i, index) => (
-                  <MoviesShow
+              data.map((i, index) =>
+                `'${params.id}'` === `'${i.id}'` ? (
+                  <Movie
                     key={index}
                     name={i.name}
                     image={i.imageOriginal ? i.imageOriginal : i.image}
                     genres={i.genres}
+                    visitSite={i.visitSite}
+                    officialSite={i.officialSite}
                     language={i.language}
                     rating={i.rating}
                     id={i.id}
                     summary={i.summary}
+                    country={i.country}
+                    status={i.status}
+                    IMDb={i.externals}
                   />
-                ))}
-              </div>
+                ) : undefined
+              )
             )
           ) : (
-            data.map((i, index) =>
-              `'${params.id}'` === `'${i.id}'` ? (
+            bestMoviesData.map((i, index) =>
+              `"${showBestMovies}"` === `"${i.id}"` ? (
                 <Movie
                   key={index}
                   name={i.name}
@@ -203,8 +262,8 @@ const GetDataMovie = () => {
             )
           )
         ) : (
-          BestMoviesData.map((i, index) =>
-            `"${ShowBestMovies}"` === `"${i.id}"` ? (
+          SaveSearchMoviesItemData.map((i, index) =>
+            `"${showSearchMoviesItem}"` === `"${i.id}"` ? (
               <Movie
                 key={index}
                 name={i.name}
@@ -231,8 +290,11 @@ const GetDataMovie = () => {
 
   function refresh() {
     let movies = document.querySelectorAll(".movie");
-    if (ShowBestMovies !== undefined) {
+    if (showBestMovies !== undefined) {
       setShowBestMovies(undefined);
+    }
+    if (showSearchMoviesItem !== undefined) {
+      setShowSearchMoviesItem(undefined);
     }
     if (movies.length > 0) {
       for (let i = 0; i < movies.length; i++) {
@@ -250,6 +312,9 @@ const GetDataMovie = () => {
     let input = document.getElementById("input");
     let url = fetch("https://api.tvmaze.com/search/shows?q=" + input.value);
     input.value = "";
+    if (showSearchMoviesItem !== undefined) {
+      setShowSearchMoviesItem(undefined);
+    }
     url
       .then(async (res) => {
         const response = await axios.get(res.url);
@@ -298,13 +363,7 @@ const GetDataMovie = () => {
       });
     window.scrollTo({ top: 0, behavior: "smooth" });
     if (input.value === "") {
-      setTimeout(() => {
-        let notFindMovieDisplayOn =
-          document.getElementById("ShowSearchMoviesOn");
-        if (notFindMovieDisplayOn !== null) {
-          notFindMovieDisplayOn.setAttribute("id", "ShowSearchMoviesOff");
-        }
-      }, 20);
+      ShowSearchMoviesOff();
     }
   }
   function pressEnter(e) {
@@ -312,30 +371,43 @@ const GetDataMovie = () => {
     if (enter === "Enter") {
       search();
       nav("/search-movies/");
-      setTimeout(() => {
-        let notFindMovieDisplayOn =
-          document.getElementById("ShowSearchMoviesOn");
-        if (notFindMovieDisplayOn !== null) {
-          notFindMovieDisplayOn.setAttribute("id", "ShowSearchMoviesOff");
-        }
-      }, 20);
+      ShowSearchMoviesOff();
+      if (showSearchMoviesItem !== undefined) {
+        setShowSearchMoviesItem(undefined);
+      }
     }
   }
   function ShowSearchMoviesOff() {
-    let notFindMovieDisplayOn = document.getElementById("ShowSearchMoviesOn");
     setTimeout(() => {
+      let notFindMovieDisplayOn = document.getElementById("ShowSearchMoviesOn");
       if (notFindMovieDisplayOn !== null) {
         notFindMovieDisplayOn.setAttribute("id", "ShowSearchMoviesOff");
       }
     }, 1);
   }
   function ShowSearchMoviesOn() {
-    let notFindMovieDisplayOff = document.getElementById("ShowSearchMoviesOff");
     setTimeout(() => {
+      let notFindMovieDisplayOff = document.getElementById(
+        "ShowSearchMoviesOff"
+      );
       if (notFindMovieDisplayOff !== null) {
         notFindMovieDisplayOff.setAttribute("id", "ShowSearchMoviesOn");
+        notFindMovieDisplayOff.scrollTo({ top: 0, behavior: "smooth" });
       }
     }, 1);
+  }
+
+  function showItem(e) {
+    if (showSearchMoviesItem !== undefined) {
+      setShowSearchMoviesItem(undefined);
+    }
+    let item = e.currentTarget;
+    let input = document.getElementById("input");
+    input.value = "";
+    setShowSearchMoviesItem(item.id);
+    ShowSearchMoviesOff();
+    let saveData = [...showSearchMovies];
+    setShowSearchMoviesItemData(saveData);
   }
 
   // Pagination
@@ -486,7 +558,6 @@ const GetDataMovie = () => {
     if (name.length > 35) {
       let slice = name.slice(0, 35);
       let add = (slice += "...");
-      console.log(add);
       return add;
     } else {
       return name;
@@ -498,13 +569,13 @@ const GetDataMovie = () => {
     let bestMoviesCount = document.querySelectorAll("#itemBestMovies");
     setTimeout(() => {
       if (bestMoviesCount.length === 0) {
-        let Data = BestMoviesData;
+        let Data = bestMoviesData;
         for (let i = 0; i < 3; i++) {
           let images = BestMovieParentElement.current;
           let createTagA = document.createElement("a");
           let createImg = document.createElement("img");
           createTagA.setAttribute("id", "itemBestMovies");
-          createImg.addEventListener("click", test);
+          createImg.addEventListener("click", bestMovieShow);
           createImg.setAttribute("src", Data[i].imageOriginal);
           createImg.setAttribute("id", Data[i].id);
           createImg.setAttribute("class", "imgShow");
@@ -517,13 +588,13 @@ const GetDataMovie = () => {
     }, 10);
   }
 
-  function test(e) {
+  function bestMovieShow(e) {
     let item = e.currentTarget;
     setShowBestMovies(item.id);
   }
 
   function btnRight() {
-    let Data = BestMoviesData;
+    let Data = bestMoviesData;
     let imgShow = document.querySelectorAll(".imgShow");
     let imgShowCenter = document.querySelectorAll(".imgShowCenter");
     for (let i = 0; i <= 6; i++) {
@@ -564,7 +635,7 @@ const GetDataMovie = () => {
   }
 
   function btnLeft() {
-    let Data = BestMoviesData;
+    let Data = bestMoviesData;
     let imgShow = document.querySelectorAll(".imgShow");
     let imgShowCenter = document.querySelectorAll(".imgShowCenter");
     for (let i = 7; i >= 0; i--) {
